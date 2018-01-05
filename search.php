de73ef8029c5d17d4ea2de84782c107c5faf881d
Joe Querin <7998323+joecue@users.noreply.github.com>
2018-01-05 14:54:24 -0500

    Search Page

--- search.php
+++ search.php
@@ -1,13 +1,20 @@
 <?php
 
 get_header();
+?>
+<div class="row">
+	<div class="small-12 columns">
+<?php	
 
-	while ( have_posts() ) : the_post();
+ while ( have_posts() ) : the_post();
 
 	 get_template_part( 'template-parts/content', 'search' );
 
  endwhile; // end of the loop.
-
+?>
+	</div>
+</div>
+	<?php
 get_footer();
 
 ?>
