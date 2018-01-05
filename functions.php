e816d4a31517ade5bb23a087fa944c49e00599be
Joe Querin <7998323+joecue@users.noreply.github.com>
2018-01-05 17:07:39 -0500

    Remove JetPack Sharing

--- functions.php
+++ functions.php
@@ -336,4 +336,17 @@ function custom_excerpt_length($length) {
     return 40;
 }
 add_filter('excerpt_length', 'custom_excerpt_length');
+
+// Removing Default Jetpack Sharing Button Filters
+
+function jptweak_remove_share() {
+    remove_filter( 'the_content', 'sharing_display',19 );
+    remove_filter( 'the_excerpt', 'sharing_display',19 );
+    if ( class_exists( 'Jetpack_Likes' ) ) {
+        remove_filter( 'the_content', array( Jetpack_Likes::init(), 'post_likes' ), 30, 1 );
+    }
+}
+ 
+add_action( 'loop_start', 'jptweak_remove_share' );
+
 ?>
