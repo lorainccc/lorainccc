659e3bad989e38194adcdd54d394eb5693c1e3c5
Joe Querin <7998323+joecue@users.noreply.github.com>
2018-02-06 13:50:04 -0500

    Tweaks for to fix mobile menu
    Mobile search widget seems to be interfering

--- header.php
+++ header.php
@@ -231,9 +231,9 @@
       </span> </div>
   </div>
   <div id="mobile-search" class="show-for-small-only">
-        <?php if ( is_active_sidebar( 'lccc-search-sidebar' ) ) { 
-																dynamic_sidebar( 'lccc-search-sidebar' ); 
-								}		?>
+        <?php 
+										the_widget('WP_Widget_Search');
+								?>
   </div>
   <div id="responsive-menu" class="show-for-small-only">
 			  <nav role="navigation" aria-label="<?php _e( 'Mobile Main Menu', 'lorainccc' );?>" aria-describedby="mobile-primary-menu mobile-header-shortcuts">
