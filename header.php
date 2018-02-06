cd31b7b88b35f80bef49e91a5fd01b506275b572
Joe Querin <7998323+joecue@users.noreply.github.com>
2018-02-06 13:43:29 -0500

    Tweaks for search widget in header

--- header.php
+++ header.php
@@ -197,22 +197,8 @@
         </label>
       </form>-->
      <div class="large-9 medium-6 columns searchbox">
-      	<?php if ( is_active_sidebar( 'lccc-search-sidebar' ) ) { ?>
-																<?php dynamic_sidebar( 'lccc-search-sidebar' ); ?>
-								<?php }else{
-															if ( class_exists( 'CustomGoogleSearch' ) ) {
-																// check for plugin using plugin name
-																// Plugin is activated
-																		if ( is_active_sidebar( 'lccc-four-o-four-sidebar' ) ) {
-
-																		}else{
-																		$instance = array(
-																			'display_results' => '3',
-																		);                                               the_widget('CGS_Widget', $instance);
-																			}
-															}else{
-																echo 'no widgets found';
-															}
+        <?php if ( is_active_sidebar( 'lccc-search-sidebar' ) ) { 
+																dynamic_sidebar( 'lccc-search-sidebar' ); 
 								}		?>
      </div>
     </div>
