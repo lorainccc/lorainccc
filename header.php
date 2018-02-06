ff4a03f75be499dc408f02a00465a8612fce3b88
Joe Querin <7998323+joecue@users.noreply.github.com>
2018-02-06 12:42:38 -0500

    Tweak to widget area for mobile search

--- header.php
+++ header.php
@@ -245,9 +245,10 @@
       </span> </div>
   </div>
   <div id="mobile-search" class="show-for-small-only">
-        <?php if ( is_active_sidebar( 'lccc-search-sidebar' ) ) { ?>
-																<?php dynamic_sidebar( 'lccc-search-sidebar' ); ?>
-								}		?>
+        <?php if ( is_active_sidebar( 'lccc-search-sidebar' ) ) { 
+																	dynamic_sidebar( 'lccc-search-sidebar' ); 
+														}
+								?>
   </div>
   <div id="responsive-menu" class="show-for-small-only">
 			  <nav role="navigation" aria-label="<?php _e( 'Mobile Main Menu', 'lorainccc' );?>" aria-describedby="mobile-primary-menu mobile-header-shortcuts">
