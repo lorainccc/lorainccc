7cd59eb5f06d722f2a5c81efc7d2ea6332b3bf5f
Joe Querin <7998323+joecue@users.noreply.github.com>
2024-06-25 13:43:53 -0400

    :wrench: Multiple Updates
    Added display=swap for Google Fonts
    Removed old GA code snippet, Added New GA4 snippet and tag manager snippets
    Added ability to add sync and defer attributes to scripts.

--- functions.php
+++ functions.php
@@ -198,8 +198,8 @@ add_action( 'widgets_init', 'lorainccc_widgets_init' );
  * Enqueue google fonts.
  */
 function add_google_fonts() {
-wp_enqueue_style( 'open-sans-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic', false );
-wp_enqueue_style( 'raleway-google-fonts', 'https://fonts.googleapis.com/css?family=Raleway:400,700', false );
+wp_enqueue_style( 'open-sans-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic&display=swap', false );
+wp_enqueue_style( 'raleway-google-fonts', 'https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap', false );
 
 }
 
@@ -230,7 +230,8 @@ function lorainccc_foundation_scripts() {
 
 	//Adds Google Analytics, Google Tag, Hotjar and Eloqua to header
 	wp_enqueue_script( 'lc-eloqua-scripts', get_stylesheet_directory_uri() . '/js/lc-eloqua.js', array(), '20180828', false);
-	wp_enqueue_script( 'lc-google-analytics-scripts', get_stylesheet_directory_uri() . '/js/lc-google-analytics.js', array(), '20180828', false);
+	wp_enqueue_script( 'lc-google-analytic-parent-async', 'https://www.googletagmanager.com/gtag/js?id=G-Z27HB3ECDG', array(), '20221117', false); 
+	wp_enqueue_script( 'lc-google-analytics-scripts', get_stylesheet_directory_uri() . '/js/lc-google-analytics.js', array( 'lc-google-analytic-parent' ), '20180828', false);
 	wp_enqueue_script( 'lc-google-tag-scripts', get_stylesheet_directory_uri() . '/js/lc-google-tag.js', array(), '20180828', false);
 	wp_enqueue_script( 'lc-hotjar-scripts', get_stylesheet_directory_uri() . '/js/lc-hotjar.js', array(), '20180828', false);
 	wp_enqueue_script( 'lc-siteimprove-scripts', get_stylesheet_directory_uri() . '/js/lc-siteimprove.js', array(), '20180828', false);
@@ -251,6 +252,35 @@ function lorainccc_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'lorainccc_scripts' );
+
+
+/**
+* Add async or defer attributes to script enqueues
+* @author Mike Kormendy
+* @param  String  $tag     The original enqueued <script src="...> tag
+* @param  String  $handle  The registered unique name of the script
+* @return String  $tag     The modified <script async|defer src="...> tag
+*/
+// only on the front-end
+if(!is_admin()) {
+    function lc_add_asyncdefer_attribute($tag, $handle) {
+        // if the unique handle/name of the registered script has 'async' in it
+        if (strpos($handle, 'async') !== false) {
+            // return the tag with the async attribute
+            return str_replace( '<script ', '<script async ', $tag );
+        }
+        // if the unique handle/name of the registered script has 'defer' in it
+        else if (strpos($handle, 'defer') !== false) {
+            // return the tag with the defer attribute
+            return str_replace( '<script ', '<script defer ', $tag );
+        }
+        // otherwise skip
+        else {
+            return $tag;
+        }
+    }
+    add_filter('script_loader_tag', 'lc_add_asyncdefer_attribute', 10, 2);
+}
 
 /**
  * Implement the Custom Header feature.
